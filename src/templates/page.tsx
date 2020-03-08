6786b4ecb1fc1b222f82a27bc4ff0ce7821771d7
Jez <jez@jezfx.com>
2020-03-08 21:30:08 +0000

    remove slice_label

--- src/templates/page.tsx
+++ src/templates/page.tsx
@@ -33,7 +33,7 @@ export const pageQuery = graphql`
                     raw
                   }
                 }
-                slice_label
+                slice_type
               }
             }
             hero {
